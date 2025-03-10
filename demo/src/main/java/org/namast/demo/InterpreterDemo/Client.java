1433fc25ad0045b572c24b98eb5108f1e32e932f
namast0601 <108456121+namast0601@users.noreply.github.com>
2025-03-10 13:24:15 +0700

    Add code demo

---
+++ demo/src/main/java/org/namast/demo/InterpreterDemo/Client.java
@@ -0,0 +1,29 @@
+package org.namast.demo.InterpreterDemo;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Client {
+
+    public static void main(String[] args) {
+        String[] romans = { "IV", "XII", "CLIX", "MMXVIII", "MMMDLIV" };
+        for (String roman : romans) {
+            convertRomanToNumber(roman);
+        }
+    }
+
+    private static void convertRomanToNumber(String roman) {
+        List<Expression> tree = new ArrayList<>();
+        tree.add(new ThousandExpression());
+        tree.add(new HundredExpression());
+        tree.add(new TenExpression());
+        tree.add(new OneExpression());
+
+        Context context = new Context(roman);
+        for (Expression exp : tree) {
+            exp.interpret(context);
+        }
+        System.out.println(roman + " = " + context.getOutput());
+    }
+}
