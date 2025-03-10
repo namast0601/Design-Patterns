a24764a5a9f37e62badc4a656f631dabee90b0da
namast0601 <108456121+namast0601@users.noreply.github.com>
2025-03-10 10:31:22 +0700

    Protection proxy

---
+++ demo/src/main/java/org/namast/demo/protection_proxy/UserService.java
@@ -0,0 +1,8 @@
+package org.namast.demo.protection_proxy;
+
+public interface UserService {
+
+    void load();
+
+    void insert();
+}
---
+++ demo/src/main/java/org/namast/demo/protection_proxy/UserServiceImpl.java
@@ -0,0 +1,21 @@
+package org.namast.demo.protection_proxy;
+
+public class UserServiceImpl implements UserService {
+
+    private String name;
+
+    public UserServiceImpl(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void load() {
+        System.out.println(name + " loaded");
+    }
+
+    @Override
+    public void insert() {
+        System.out.println(name + " inserted");
+    }
+
+}
